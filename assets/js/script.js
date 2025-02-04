4e62ebb172b3f8804343e423b638279c1063ec58
Daniel <Daniel.halaas@gmail.com>
2025-02-04 12:04:48 +0100

    added darkmode toggle functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,3 +43,10 @@ function watchForHover() {
 }
 
 watchForHover()
+
+
+// function to toggle dark mode
+function toggleDarkMode() {
+    var element = document.body;
+    element.classList.toggle('dark-mode');
+}
